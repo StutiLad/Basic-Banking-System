a93c46036351454b60ea1ac4541847d171969b65
StutiLad <72381827+StutiLad@users.noreply.github.com>
2021-04-17 21:10:23 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
-	$dbname = "tsf-bank";
+	$dbname = "tsf_bank";
 
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
 
